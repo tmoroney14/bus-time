b064b814adfe8e05bfbcdc3d28a9172343c17e9f
tmoroney14 <thomas.moroney1010@gmail.com>
2019-10-20 22:06:53 +0100

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Component({
   selector: 'my-app',
@@ -8,16 +7,4 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 })
 export class AppComponent  {
 
-  url = `https://data.smartdublin.ie/cgi-bin/rtpi/timetableinformation?type=week&stopid=5165&routeid=33b&format=json`;
-  items = [];
-
-  constructor(private http: HttpClient) {
-    this.http.get(this.url).toPromise().then(data => {
-      console.log(data);
-
-      for (let key in data)
-        if (data.hasOwnProperty(key))
-          this.items.push(data[key]);
-    });
-}
 }
--- src/app/bus-stop/bus-stop.component.ts
+++ src/app/bus-stop/bus-stop.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Component({
   selector: 'app-bus-stop',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BusStopComponent implements OnInit {
 
-  constructor() { }
+    url = `https://data.smartdublin.ie/cgi-bin/rtpi/timetableinformation?type=week&stopid=5165&routeid=33b&format=json`;
+  items = [];
+
+  constructor(private http: HttpClient) {
+    this.http.get(this.url).toPromise().then(data => {
+      console.log(data);
+
+      for (let key in data)
+        if (data.hasOwnProperty(key))
+          this.items.push(data[key]);
+    });
+  }
 
   ngOnInit() {
   }
-
 }
